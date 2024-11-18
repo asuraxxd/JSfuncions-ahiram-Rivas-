07fdf41967b2e8c2255fd7fabc16447523e4d6c9
asuraxxd <ahiram.rivas@institutvidreres.cat>
2024-11-18 17:45:37 +0100

    Exercici 4

--- JSfuncions_ahiram_rivas.js
+++ JSfuncions_ahiram_rivas.js
@@ -16,3 +16,13 @@ function mostrarDataActual() {
     console.log(`Data actual: ${new Date()}`);
 }
 mostrarDataActual();
+
+//exercici 4
+function declararVariable() {
+    let variableLocal = "Aquest és un valor local";
+    console.log(variableLocal);
+}
+declararVariable();
+// console.log(variableLocal); // Error: variableLocal is not defined
+
+//Explicació: Les variables declarades dins d'una funció només existeixen en el seu àmbit (scope).
